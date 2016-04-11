02dc399e1f35644c0fce4e5c967e07f222c02991
Andrew Edgar <aedgar@ca.ibm.com>
2016-04-11 14:14:21 -0700

    Update pid_utils to the latest version
    * modify rep_ctl to handle bosh output/exit messaging in a bash trap
    
    [#116066759]
    
    Signed-off-by: James Myers <jmyers@pivotal.io>

--- src/pid_utils.sh
+++ src/pid_utils.sh
@@ -6,82 +6,126 @@ exec 2>> /var/vcap/sys/log/monit/$SCRIPT.err.log
 
 echo "------------ `basename $0` $* at `date` --------------" | tee /dev/stderr
 
-pid_guard() {
-  pidfile=$1
-  name=$2
-
-  if [ -f "$pidfile" ]; then
-    pid=$(head -1 "$pidfile")
-
-    if [ -n "$pid" ] && [ -e /proc/$pid ]; then
-      echo "$name is already running, please stop it first"
-      exit 1
-    fi
-
-    echo "Removing stale pidfile..."
-    rm $pidfile
-  fi
+function pid_is_running() {
+  declare pid="$1"
+  ps -p "${pid}" >/dev/null 2>&1
 }
 
-wait_pidfile() {
-  pidfile=$1
-  try_kill=$2
-  timeout=${3:-0}
-  force=${4:-0}
-  countdown=$(( $timeout * 10 ))
+# pid_guard
+#
+# @param pidfile
+# @param name [String] an arbitrary name that might show up in STDOUT on errors
+#
+# Run this before attempting to start new processes that may use the same :pidfile:.
+# If an old process is running on the pid found in the :pidfile:, exit 1. Otherwise,
+# remove the stale :pidfile: if it exists.
+#
+function pid_guard() {
+  declare pidfile="$1" name="$2"
 
-  if [ -f "$pidfile" ]; then
-    pid=$(head -1 "$pidfile")
+  echo "------------ STARTING $(basename "$0") at $(date) --------------" | tee /dev/stderr
 
-    if [ -z "$pid" ]; then
-      echo "Unable to get pid from $pidfile"
-      exit 1
+  if [ ! -f "${pidfile}" ]; then
+    return 0
+  fi
+
+  local pid
+  pid=$(head -1 "${pidfile}")
+
+  if pid_is_running "${pid}"; then
+    echo "${name} is already running, please stop it first"
+    exit 1
+  fi
+
+  echo "Removing stale pidfile"
+  rm "${pidfile}"
+}
+
+# wait_pid_death
+#
+# @param pid
+# @param timeout
+#
+# Watch a :pid: for :timeout: seconds, waiting for it to die.
+# If it dies before :timeout:, exit 0. If not, exit 1.
+#
+# Note that this should be run in a subshell, so that the current
+# shell does not exit.
+#
+function wait_pid_death() {
+  declare pid="$1" timeout="$2"
+
+  local countdown
+  countdown=$(( timeout * 10 ))
+
+  while true; do
+    if ! pid_is_running "${pid}"; then
+      return 0
     fi
 
-    if [ -e /proc/$pid ]; then
-      if [ "$try_kill" = "1" ]; then
-        echo "Killing $pidfile: $pid "
-        kill $pid
-      fi
-      while [ -e /proc/$pid ]; do
-        sleep 0.1
-        [ "$countdown" != '0' -a $(( $countdown % 10 )) = '0' ] && echo -n .
-        if [ $timeout -gt 0 ]; then
-          if [ $countdown -eq 0 ]; then
-            if [ "$force" = "1" ]; then
-              echo -ne "\nKill timed out, using kill -9 on $pid... "
-              kill -9 $pid
-              sleep 0.5
-            fi
-            break
-          else
-            countdown=$(( $countdown - 1 ))
-          fi
-        fi
-      done
-      if [ -e /proc/$pid ]; then
-        echo "Timed Out"
-      else
-        echo "Stopped"
-      fi
-    else
-      echo "Process $pid is not running"
+    if [ ${countdown} -le 0 ]; then
+      return 1
     fi
 
-    rm -f $pidfile
+    countdown=$(( countdown - 1 ))
+    sleep 0.1
+  done
+}
+
+# kill_and_wait
+#
+# @param pidfile
+# @param timeout [default 25s]
+#
+# For a pid found in :pidfile:, send a `kill -6`, then wait for :timeout: seconds to
+# see if it dies on its own. If not, send it a `kill -9`. If the process does die,
+# exit 0 and remove the :pidfile:. If after all of this, the process does not actually
+# die, exit 1.
+#
+# Note:
+# Monit default timeout for start/stop is 30s
+# Append 'with timeout {n} seconds' to monit start/stop program configs
+#
+function kill_and_wait() {
+  declare pidfile="$1" timeout="${2:-25}" sigkill_on_timeout="${3:-1}"
+
+  if [ ! -f "${pidfile}" ]; then
+    echo "Pidfile ${pidfile} doesn't exist"
+    exit 0
+  fi
+
+  local pid
+  pid=$(head -1 "${pidfile}")
+
+  if [ -z "${pid}" ]; then
+    echo "Unable to get pid from ${pidfile}"
+    exit 1
+  fi
+
+  if ! pid_is_running "${pid}"; then
+    echo "Process ${pid} is not running"
+    rm -f "${pidfile}"
+    exit 0
+  fi
+
+  echo "Killing ${pidfile}: ${pid} "
+  kill "${pid}"
+
+  if ! wait_pid_death "${pid}" "${timeout}"; then
+    if [ "${sigkill_on_timeout}" = "1" ]; then
+      echo "Kill timed out, using kill -9 on ${pid}"
+      kill -9 "${pid}"
+      sleep 0.5
+    fi
+  fi
+
+  if pid_is_running "${pid}"; then
+    echo "Timed Out"
+    exit 1
   else
-    echo "Pidfile $pidfile doesn't exist"
+    echo "Stopped"
+    rm -f "${pidfile}"
   fi
-}
-
-kill_and_wait() {
-  pidfile=$1
-  # Monit default timeout for start/stop is 30s
-  # Append 'with timeout {n} seconds' to monit start/stop program configs
-  timeout=${2:-25}
-  force=${3:-1}
-
-  wait_pidfile $pidfile 1 $timeout $force
 }
 
 check_mount() {
