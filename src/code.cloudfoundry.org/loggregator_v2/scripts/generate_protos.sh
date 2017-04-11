01e439946a50c733f2448c87ebc48b738f8d4e9d
Eno Compton <ecompton@pivotal.io>
2017-04-11 12:16:08 -0600

    Extract code.cloudfoundry.org/loggregator_v2
    The loggregator client is now submoduled as:
    code.cloudfoundry.org/go-loggregator.
    
    Submodule src/code.cloudfoundry.org/go-loggregator 00000000...c29540cb (new
    submodule)
    Submodule src/code.cloudfoundry.org/executor 3c8d794e..459e213e:
      > Update import path to use go-loggregator
    Submodule src/code.cloudfoundry.org/inigo d9d59c58..6e2f3cef:
      > Update import path to use go-loggregator
    Submodule src/code.cloudfoundry.org/rep 741326070..7710507d7:
      > Update import path to use go-loggregator
    Submodule src/code.cloudfoundry.org/volman e4c007f87..8c99d61d3:
      > Update import path to use go-loggregator
    
    Signed-off-by: Warren Fernandes <warren.f.fernandes@gmail.com>

--- src/code.cloudfoundry.org/loggregator_v2/scripts/generate_protos.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-rm $GOPATH/bin/protoc-gen-gogoslick
-go install -v github.com/gogo/protobuf/protoc-gen-gogoslick
-
-protoc --proto_path=../loggregator-api/v2 --gogoslick_out=plugins=grpc:. ../loggregator-api/v2/*.proto
