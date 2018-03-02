5a76235e89017d79d1635f0d0c82c5b4ffe3116c
Nima Kaviani <nkavian@us.ibm.com>
2018-03-01 16:16:24 -0800

    cleanup scripts after removing cf-release
    Signed-off-by: Edwin Xie <exie@pivotal.io>

--- scripts/get_paths.sh
+++ scripts/get_paths.sh
@@ -5,14 +5,5 @@ absolute_path() {
 }
 
 scripts_path=$(absolute_path `dirname $0`)
-
 DIEGO_RELEASE_DIR=${DIEGO_RELEASE_DIR:-$(absolute_path $scripts_path/..)}
-CF_RELEASE_DIR=${CF_RELEASE_DIR:-$(absolute_path $DIEGO_RELEASE_DIR/../cf-release)}
-
-CF_MANIFESTS_DIR=$CF_RELEASE_DIR/bosh-lite/deployments
-DIEGO_MANIFESTS_DIR=$DIEGO_RELEASE_DIR/bosh-lite/deployments
-
 echo DIEGO_RELEASE_DIR=$DIEGO_RELEASE_DIR
-echo CF_RELEASE_DIR=$CF_RELEASE_DIR
-echo CF_MANIFESTS_DIR=$CF_MANIFESTS_DIR
-echo DIEGO_MANIFESTS_DIR=$DIEGO_MANIFESTS_DIR
