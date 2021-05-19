e740cc6d6d60bbf9bbb846b752cfad9d1bb79d35
Josh Russett <jrussett@vmware.com>
2021-05-19 18:03:18 +0000

    ci: run ginkgo based on mod version/don't install
    - try to remove a lot of GOPATH references
    - uses the vendored ginkgo instead of `go get`ing it again
    - running unit tests requires DIEGO_RELEASE_DIR env variable, but this
    is automatically setup via scripts
    - updates some of the scripts to be more sane
    - corrects nats-server installation
    - Also prints debug info on the container
    
    [#177498057](https://www.pivotaltracker.com/story/show/177498057)

--- scripts/ci/run_unit_windows.ps1
+++ scripts/ci/run_unit_windows.ps1
@@ -1,44 +1,32 @@
 ﻿$ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
+$env:DIEGO_RELEASE_DIR = Resolve-Path -Path $pwd/diego-release/ | select -ExpandProperty Path
 cd diego-release/
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$CONSUL_DIR = "C:/consul"
+$CONSUL_DIR = "C:\consul"
 # Remove-Item $CONSUL_DIR -Force
 if(!(Test-Path -Path $CONSUL_DIR )) {
     New-Item -ItemType directory -Path $CONSUL_DIR
     (New-Object System.Net.WebClient).DownloadFile('https://releases.hashicorp.com/consul/0.7.0/consul_0.7.0_windows_amd64.zip', "$CONSUL_DIR/consul.zip")
     [System.IO.Compression.ZipFile]::ExtractToDirectory("$CONSUL_DIR/consul.zip", "$CONSUL_DIR")
 }
-
-$env:GOPATH=($pwd).path
-$env:GOBIN=$env:GOPATH + "/bin"
-$env:PATH = $env:GOBIN +  ";" + $CONSUL_DIR + ";" + $env:PATH
-# Write-Host "Gopath is " + $env:GOPATH
-# Write-Host "PATH is " + $env:PATH
+$env:PATH += ";$CONSUL_DIR"
 
 Write-Host "Installing nats-server"
-
-$NATS_DIR = "C:/nats-server"
-# Remove-Item $NATS_DIR -Force
+$NATS_DIR = "C:\nats-server"
+Remove-Item -Force -Recurse -ErrorAction Ignore $NATS_DIR
 if(!(Test-Path -Path $NATS_DIR )) {
   New-Item -ItemType directory -Path $NATS_DIR
-  (New-Object System.Net.WebClient).DownloadFile('https://github.com/nats-io/nats-server/releases/download/v2.1.2/nats-server-v2.1.2-windows-amd64.zip', "nats-server.zip")
-    [System.IO.Compression.ZipFile]::ExtractToDirectory("nats-server.zip", "$NATS_DIR")
-    Move-Item -Path "$NATS_DIR/*/nats-server.exe" "$env:GOPATH/bin/nats-server.exe"
+  (New-Object System.Net.WebClient).DownloadFile('https://github.com/nats-io/nats-server/releases/download/v2.1.2/nats-server-v2.1.2-windows-amd64.zip', "$NATS_DIR/nats-server.zip")
+  [System.IO.Compression.ZipFile]::ExtractToDirectory("$NATS_DIR/nats-server.zip", "$NATS_DIR")
+  Copy-Item -Path "$NATS_DIR/nats-server-*/nats-server.exe" -Destination "$NATS_DIR/nats-server.exe"
 }
+$env:PATH += ";$NATS_DIR"
 $env:NATS_DOCKERIZED = "1"
-
-Write-Host "Installing Ginkgo"
-go get github.com/onsi/ginkgo/ginkgo
-if ($LastExitCode -ne 0) {
-    throw "Ginkgo installation process returned error code: $LastExitCode"
-} else {
-  Write-Host "Ginkgo successfully installed"
-}
 
 Write-Host "Downloading winpty DLL"
 $WINPTY_DIR = "C:\winpty"
@@ -49,12 +37,19 @@ if(!(Test-Path -Path $WINPTY_DIR )) {
 }
 $env:WINPTY_DLL_DIR="$WINPTY_DIR\x64\bin"
 
+
+Write-Host "CONTAINER DEBUG INFO"
+Write-Host "DIEGO_RELEASE_DIR = $env:DIEGO_RELEASE_DIR"
+Write-Host "PATH = $env:PATH"
+
+
+
 Write-Host "Running store-independent test suites against a MySQL database..."
 $env:SQL_FLAVOR="mysql"
 
 cd src/code.cloudfoundry.org/
 
-ginkgo -r -keepGoing -trace -randomizeAllSpecs -progress -race `
+go run github.com/onsi/ginkgo/ginkgo -r -keepGoing -trace -randomizeAllSpecs -progress -race `
   route-emitter `
   cfhttp `
   cacheddownloader `
--- scripts/get_paths.sh
+++ scripts/get_paths.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-absolute_path() {
-  (cd $1 && pwd)
-}
-
-scripts_path=$(absolute_path `dirname $0`)
-DIEGO_RELEASE_DIR=${DIEGO_RELEASE_DIR:-$(absolute_path $scripts_path/..)}
-echo DIEGO_RELEASE_DIR=$DIEGO_RELEASE_DIR
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+DIEGO_RELEASE_DIR=${DIEGO_RELEASE_DIR:-$(realpath ${SCRIPT_DIR}/..)}
+export DIEGO_RELEASE_DIR=$DIEGO_RELEASE_DIR
