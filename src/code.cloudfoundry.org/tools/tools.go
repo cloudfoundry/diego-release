88aaaccca928ef1b444e96eaa37d40bc8210a761
Amin Jamali <ajamali@pivotal.io>
2021-06-01 16:07:14 +0000

    Install gogoslick from module
    instead of `go get`ing it

--- src/code.cloudfoundry.org/tools/tools.go
+++ src/code.cloudfoundry.org/tools/tools.go
@@ -3,9 +3,10 @@
 package tools
 
 import (
+	_ "github.com/gogo/protobuf/protoc-gen-gogoslick"
+	_ "github.com/hashicorp/consul"
 	_ "github.com/nats-io/nats-server"
 	_ "github.com/onsi/ginkgo/ginkgo"
-	_ "github.com/hashicorp/consul"
 )
 
 // This file imports packages that are used when running go generate, or used
