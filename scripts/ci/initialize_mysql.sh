464a6f6560063a5fa8a8d49edabc784a7f32ecf1
Carson Long <lcarson@vmware.com>
2021-10-12 14:50:07 +0000

    bump bbs locket
    Submodule src/code.cloudfoundry.org/bbs ead33eef4..97e4a3a95:
      > Make Database users configurable
    Submodule src/code.cloudfoundry.org/locket cc319aa2c..0dd2ac392:
      > Make database user configurable
      > Add certauthority helper package

--- scripts/ci/initialize_mysql.sh
+++ scripts/ci/initialize_mysql.sh
@@ -13,3 +13,48 @@ EOF
   sed -i "s/datadir.*/datadir=${escaped_datadir}/g" /etc/mysql/mysql.conf.d/mysqld.cnf
   service mysql start
 }
+
+function bootDB {
+  db=$1
+
+  if [ "$db" = "random" ]; then
+    cointoss=$RANDOM
+    set +e
+    let "cointoss %= 2"
+    set -e
+    if [ "$cointoss" == "0" ]; then
+      db="postgres"
+    else
+      db="mysql"
+    fi
+  fi
+
+  if [ "$db" = "postgres" ]; then
+    launchDB="(docker-entrypoint.sh -c max_connections=300 &> /var/log/postgres-boot.log) &"
+    testConnection="psql -h localhost -U $POSTGRES_USER -c '\conninfo' &>/dev/null"
+  elif [[ "$db" == "mysql"* ]]; then
+    chown -R mysql:mysql /var/run/mysqld
+    launchDB="(MYSQL_USER='' MYSQL_ROOT_PASSWORD=$MYSQL_PASSWORD /entrypoint.sh mysqld &> /var/log/mysql-boot.log) &"
+    testConnection="echo '\s;' | mysql -h127.0.0.1 -uroot --password=$MYSQL_PASSWORD &>/dev/null"
+  else
+    echo "skipping database"
+    return 0
+  fi
+
+  echo -n "booting $db"
+  eval "$launchDB"
+  for _ in $(seq 1 60); do
+    set +e
+    eval "${testConnection}"
+    exitcode=$?
+    set -e
+    if [ ${exitcode} -eq 0 ]; then
+      echo "connection established to $db"
+      return 0
+    fi
+    echo -n "."
+    sleep 1
+  done
+  echo "unable to connect to $db"
+  exit 1
+}
