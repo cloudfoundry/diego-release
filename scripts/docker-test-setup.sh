84a808257bb4b31f2b42c2466836121a2fcddc81
Josh Russett <jrussett@vmware.com>
2022-11-07 21:15:43 +0000

    Make sure NATS is installed
    Signed-off-by: Brandon Roberson <broberson@vmware.com>

--- scripts/docker-test-setup.sh
+++ scripts/docker-test-setup.sh
@@ -15,6 +15,17 @@ if ! [ $(type -P "ginkgo") ]; then
   mv /root/go/bin/ginkgo /usr/local/bin/ginkgo
 fi
 
+
+if ! [ $(type -P "nats-server") ]; then
+  BIN_DIR="${DIEGO_RELEASE_DIR}/bin"
+  mkdir -p "${BIN_DIR}"
+  export PATH="${PATH}:${BIN_DIR}"
+  export GOFLAGS="-buildvcs=false"
+  pushd "${DIEGO_RELEASE_DIR}/src/code.cloudfoundry.org"
+    go build -o "$BIN_DIR/nats-server" github.com/nats-io/nats-server/v2
+  popd
+fi
+
 bootDB "${DB:-"notset"}"
 set +e
 exec /bin/bash "$@"
