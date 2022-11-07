cc45875f63fc8240e6515bce4198ee6afb5e37f6
Brandon Roberson <broberson@vmware.com>
2022-11-07 21:17:02 +0000

    Update ignored packages and add default postgres creds
    Signed-off-by: Josh Russett <jrussett@vmware.com>

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+set -x
+
 specified_package="${1}"
+shift
 
 set -e -u
 
 declare -a serial_packages=()
 
 declare -a ignored_packages=(
-  "src/code.cloudfoundry.org/auction/simulation"
+  "auction/simulation"
+  "benchmarkbbs"
+  "credhub-cli"
+  "routing-api"
+  "inigo"
+  "cf-tcp-touer"
+  "diego-upgrade-stability-tests"
+  "diegocanaryapp"
+  "dropsonde"
+  "go-loggregator"
+  "gorouter"
+  "vizzini"
+  "systemcerts"
 )
 
 containsElement() {
@@ -35,10 +50,15 @@ test_package() {
 if [ "${DB}" = "mysql" ]  || [ "${DB}" = "mysql-5.6" ] || [ "${DB}" = "mysql8" ]; then
   export MYSQL_USER="root"
   export MYSQL_PASSWORD="password"
+elif [ "${DB}" = "postgres" ]; then
+  export POSTGRES_USER="postgres"
+  export POSTGRES_PASSWORD="password"
 fi
 
 declare -a packages
-mapfile -t packages < <(find src -type f -name '*_test.go' -print0 | xargs -0 -L1 -I{} dirname {} | sort -u)
+pushd $DIEGO_RELEASE_DIR/src/code.cloudfoundry.org &>/dev/null
+  mapfile -t packages < <(find . -type f -name '*_test.go' -print0 | xargs -0 -L1 -I{} dirname {} | sort -u)
+popd &>/dev/null
 
 # filter out serial_packages from packages
 for i in "${serial_packages[@]}"; do
@@ -51,22 +71,8 @@ for i in "${ignored_packages[@]}"; do
   serial_packages=("${serial_packages[@]//*$i*}")
 done
 
-if [[ -z "${specified_package}" ]]; then
-  echo "testing packages: " "${packages[@]}"
-  for dir in "${packages[@]}"; do
-    test_package "${dir}" -p
-  done
-  echo "testing serial packages: " "${serial_packages[@]}"
-  for dir in "${serial_packages[@]}"; do
-    test_package "${dir}"
-  done
-else
-  specified_package="${specified_package#./}"
-  if containsElement "${specified_package}" "${serial_packages[@]}"; then
-    echo "testing serial package ${specified_package}"
-    test_package "${specified_package}"
-  else
-    echo "testing package ${specified_package}"
-    test_package "${specified_package}" -p
-  fi
-fi
+pushd $DIEGO_RELEASE_DIR/src/code.cloudfoundry.org
+  ginkgo --race -randomizeAllSpecs -randomizeSuites -failFast -p \
+    -ldflags="extldflags=-WL,--allow-multiple-definition" \
+    "${packages[@]}" "${@}"
+popd
