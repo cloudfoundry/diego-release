579640acfd9fe4353b21ada1e47eff430e783d92
i583051 <k.lapkov@sap.com>
2023-03-21 07:47:55 +0200

    Add locket config and template tests

---
+++ spec/bbs_template_spec.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+# rubocop: disable Layout/LineLength
+# rubocop: disable Metrics/BlockLength
+require 'rspec'
+require 'json'
+require 'bosh/template/test'
+
+describe 'bbs' do 
+    let(:release_path) { File.join(File.dirname(__FILE__), '..') }
+    let(:release) { Bosh::Template::Test::ReleaseDir.new(release_path)}
+    let(:job) { release.job('bbs') }
+
+    describe 'bbs.json.erb' do 
+        let(:deployment_manifest_fragment) do
+            {
+                'bpm' => {
+                    'enabled' => 'true'
+                },
+                'diego' => {
+                    'bbs' => {
+                        'active_key_label' => 'ACTIVE KEY',
+                        'detect_consul_cell_registrations' => 'false',
+                        'encryption_keys' => [
+                            'label' => 'KEY LABEL',
+                            'passphrase' => 'PASSPHRASE',  
+                        ],
+                        'sql' => {
+                            'db_host' => 'sql-db.service.cf.internal',
+                            'db_port' => 3306,
+                            'db_schema' => 'diego',
+                            'db_username' => 'diego',
+                            'db_password' => 'DB PASSWORD', 
+                            'db_driver' => 'mysql', 
+                            'ca_cert' => 'CA CERT', 
+                            'require_ssl' => true
+                        },
+                        'ca_cert' => 'CA CERT', 
+                        'auctioneer' => {
+                            'ca_cert' => 'CA CERT',
+                            'client_cert' => 'CLIENT CERT', 
+                            'client_key' => 'CLIENT KEY'
+                        },
+                        'locket' => {
+                            'client_keepalive_time' => 10,
+                            'client_keepalive_timeout' => 22
+                        },
+                        'server_cert' => 'SERVER CERT',
+                        'server_key' => 'SERVER KEY',
+                        'skip_consul_lock' => 'true',
+                        'rep' => {
+                            'require_tls' => 'true',
+                            'ca_cert' => 'CA CERT', 
+                            'client_cert' => 'CLIENT CERT', 
+                            'client_key' => 'CLIENT KEY'
+                        }
+                    },
+                    'enable_consul_service_registration' => 'false',
+                    'loggregator' => {
+                        'use_v2_api' => 'true',
+                        'ca_cert' => 'CA CERT', 
+                        'client_cert' => 'CLIENT CERT', 
+                        'client_key' => 'CLIENT KEY'
+                    }, 
+                    'logging' => {
+                        'format' => {
+                            'timestamp' => 'rfc3339'
+                        }
+                    }
+                }  
+            }
+        end
+
+        let(:template) { job.template('config/bbs.json') }
+        let(:rendered_template) { template.render(deployment_manifest_fragment) }
+
+        context 'check if locket keepalive time is bigger than the timeout' do 
+            it 'fails if the keepalive time is bigger than timeout' do
+                deployment_manifest_fragment['diego']['bbs']['locket']['client_keepalive_time'] = 23
+                expect do 
+                    rendered_template
+                end.to raise_error(/The locket client keepalive time property should not be larger than the timeout/)
+            end
+        end
+    end 
+end
