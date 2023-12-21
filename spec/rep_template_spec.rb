78f33ace50a11280407c533538fbc85f3c2169ee
i583051 <k.lapkov@sap.com>
2023-12-22 00:45:12 +0200

    Make max_containers configurable

--- spec/rep_template_spec.rb
+++ spec/rep_template_spec.rb
@@ -10,66 +10,68 @@ describe 'rep' do
   let(:release) { Bosh::Template::Test::ReleaseDir.new(release_path) }
   let(:job) { release.job('rep') }
 
-  describe 'rep.json.erb' do
-    let(:deployment_manifest_fragment) do
-      {
-        'bpm' => {
-          'enabled' => 'true'
+  let(:deployment_manifest_fragment) do
+    {
+      'bpm' => {
+        'enabled' => 'true'
+      },
+      'diego' => {
+        'executor' => {
+          'instance_identity_ca_cert' => 'CA CERT',
+          'instance_identity_key' => 'CA KEY'
         },
-        'diego' => {
-          'executor' => {
-            'instance_identity_ca_cert' => 'CA CERT',
-            'instance_identity_key' => 'CA KEY'
+        'rep' => {
+          'max_containers' => 250,
+          'locket' => {
+            'client_keepalive_time' => 10,
+            'client_keepalive_timeout' => 22
           },
-          'rep' => {
-            'locket' => {
-              'client_keepalive_time' => 10,
-              'client_keepalive_timeout' => 22
-            },
-            'preloaded_rootfses' => %w[
-              cflinuxfs3
-              cflinuxfs4
-            ]
-          }
-        },
-        'containers' => {
-          'proxy' => {
-            'enabled' => 'true',
-            'require_and_verify_client_certificates' => 'true',
-            'trusted_ca_certificates' => [
-              'GOROUTER CA',
-              'SSH PROXY CA'
-            ],
-            'verify_subject_alt_name' => [
-              'gorouter.service.cf.internal',
-              'ssh-proxy.service.cf.internal'
-            ]
-          },
-          'trusted_ca_certificate' => [
-            'DIEGO INSTANCE CA',
-            'CREDHUB CA',
-            'UAA CA'
+          'preloaded_rootfses' => %w[
+            cflinuxfs3
+            cflinuxfs4
+          ]
+        }
+      },
+      'containers' => {
+        'proxy' => {
+          'enabled' => 'true',
+          'require_and_verify_client_certificates' => 'true',
+          'trusted_ca_certificates' => [
+            'GOROUTER CA',
+            'SSH PROXY CA'
+          ],
+          'verify_subject_alt_name' => [
+            'gorouter.service.cf.internal',
+            'ssh-proxy.service.cf.internal'
           ]
         },
-        'enable_consul_service_registration' => 'false',
-        'enable_declarative_healthcheck' => 'true',
-        'loggregator' => 'LOGREGATOR PROPS',
-        'tls' => {
-          'ca_cert' => 'CA CERT',
-          'cert' => 'CERT',
-          'key' => 'KEY'
-        },
-        'logging' => {
-          'format' => {
-            'timestamp' => 'rfc3339'
-          }
+        'trusted_ca_certificate' => [
+          'DIEGO INSTANCE CA',
+          'CREDHUB CA',
+          'UAA CA'
+        ]
+      },
+      'enable_consul_service_registration' => 'false',
+      'enable_declarative_healthcheck' => 'true',
+      'loggregator' => 'LOGREGATOR PROPS',
+      'tls' => {
+        'ca_cert' => 'CA CERT',
+        'cert' => 'CERT',
+        'key' => 'KEY'
+      },
+      'logging' => {
+        'format' => {
+          'timestamp' => 'rfc3339'
         }
       }
-    end
+    }
+  end
+  
+  let(:rendered_template) { template.render(deployment_manifest_fragment) }
 
+  describe 'rep.json.erb' do
     let(:template) { job.template('config/rep.json') }
-    let(:rendered_template) { template.render(deployment_manifest_fragment) }
-
+    
     context 'check if locket keepalive time is bigger than the timeout' do
       it 'fails if the keepalive time is bigger than timeout' do
         deployment_manifest_fragment['diego']['rep']['locket']['client_keepalive_time'] = 23
@@ -79,4 +81,17 @@ describe 'rep' do
       end
     end
   end
+
+  describe 'setup_mounted_data_dirs.erb' do
+    let(:template) { job.template('bin/setup_mounted_data_dirs') }
+   
+    context 'checks the max_containers value' do 
+      it 'raises an error if max_containers is <= 0' do
+        deployment_manifest_fragment['diego']['rep']['max_containers'] = -10
+        expect do 
+          rendered_template
+        end.to raise_error(/The max_containers prop should be a positive integer/)
+      end
+    end
+  end  
 end
