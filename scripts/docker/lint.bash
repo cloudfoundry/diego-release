f84979e0f9bb4741f9b2c42c914c7a4565cf63c9
Amin Jamali <amin.jamali@broadcom.com>
2024-09-25 19:33:23 +0000

    add missing default params for linters

--- scripts/docker/lint.bash
+++ scripts/docker/lint.bash
@@ -5,6 +5,7 @@ set -o pipefail
 
 . <(/ci/shared/helpers/extract-default-params-for-task.bash /ci/shared/tasks/lint-repo/linux.yml)
 
+export DEFAULT_PARAMS="/ci/$REPO_NAME/default-params/lint-repo/linux.yml"
 pushd / > /dev/null
 /ci/shared/tasks/lint-repo/task.bash
 popd > /dev/null
